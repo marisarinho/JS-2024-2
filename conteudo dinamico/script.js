ca5a0ff643c2b1226a7ade30f044c6dd8fdc078f
marisarinho <sarinho.mariana@academico.ifpb.edu.br>
2024-12-04 11:14:42 -0300

    começando a atividade

---
+++ conteudo dinamico/script.js
@@ -0,0 +1,6 @@
+const paises = [
+    { nome: "Brasil", bandeira: "https://flagcdn.com/w320/br.png" },
+    { nome: "Argentina", bandeira: "https://flagcdn.com/w320/ar.png" },
+    { nome: "Espanha", bandeira: "https://flagcdn.com/w320/es.png" },
+    { nome: "França", bandeira: "https://flagcdn.com/w320/fr.png" },
+];
